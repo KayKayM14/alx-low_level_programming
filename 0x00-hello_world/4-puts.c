09b43c9b8abad42d65bb127c9249e27aa8cade17
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-10-05 16:19:54 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-* main - this is the main function
-*
-* Return always 0
-*/
-
-#!/bin/bash
-init main(void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"); return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
