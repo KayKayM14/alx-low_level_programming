801b7acd9fd58a9c8e69a0df865cb61f7ead5db6
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-10-13 17:55:30 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
-
 /**
-* print_numbers - print 0-9
-* return: void
+* print_numbers - print numbers
+* Return: void
 */
 
 void print_numbers(void)
 {
 int i;
-for (i = 0; c <= 10; i++)
+
+for (i = 0; i <= 10; i++)
+{
 	_putchar(i + '0');
+}
 	_putchar('\n');
 }
 
