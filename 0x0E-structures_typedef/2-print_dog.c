8066392321597e1cd42e6ea4afb25c3cf7fbc75a
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-11-07 08:24:59 -0800

    2. A dog will teach you unconditional love.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,8 +4,6 @@
 /**
  * print_dog - prints the members of a struct dog
  * @d: Pointer to the struct dog to print
- * Description: This function prints the name, age and ower of a struct dog
- * If any member is NULL, it prints "(nil)" instead of a member value
  */
 void print_dog(struct dog *d)
 {
