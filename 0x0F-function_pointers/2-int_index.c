28cb826172d0f24712685291dfbda1f5dce2649c
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-11-11 00:52:33 -0800

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,26 +2,25 @@
 #include <stdio.h>
 
 /**
- * int_index - searches for an integer
- * @array: array to search from
- * @size: size pf array
- * @cmp: function pointers
- *
- * Return: index of the matched
- */
+* int_index - searches for an integer
+* @array: array to search from
+* @size: size pf array
+* @cmp: function pointers
+*
+* Return: index of the matched
+*/
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i] == 1))
-				return (i);
-	}
-	return (-1);
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
 }
