92daf4aaf025c15c2a15af590e69e26ba647ed64
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-11-22 15:58:48 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * factorial - returns the factorial of a number
@@ -12,5 +13,6 @@ int factorial(int n)
 		return (-1);
 	if (n == 0)
 		return (1);
+
 	return (n * factorial(n - 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -12,3 +15,4 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _strlen_recursion(char *s);
 
+#endif
