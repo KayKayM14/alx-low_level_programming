c172e2d597ff4b0864f1ff7bb905eea15d3cb2b6
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-11-21 21:27:50 -0800

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint- Iterates through a list of integers and prints each element.
+ * @h: A pointer to the head of the list.
+ *
+ * Returns: Returns the total number of nodes in the list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+	const listint_t *node = h;
+
+	/*Travers the node*/
+	for (node = h; node != NULL; node = node->next)
+	{
+		printf("%d\n", node->n);
+		nodes++;
+
+	}
+
+return (nodes);
+}
