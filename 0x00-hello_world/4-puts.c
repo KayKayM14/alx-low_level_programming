333369f7ae57c2efd15445f8392bbcb1d67388b9
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-06-21 16:56:14 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * puts - prints out the string
+ * the slash adds in the entire string
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-	puts(\"Programming is like building a multilingual puzzle)
-
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
