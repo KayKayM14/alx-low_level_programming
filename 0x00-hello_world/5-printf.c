1cc03bd7a24c6d2ed00ea0234db22dc09e8d8622
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-06-21 17:25:24 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * printf - prints the string
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
