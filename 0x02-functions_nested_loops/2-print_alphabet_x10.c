13351ab010c8f90d1a7bc8c09407a203a76d0c8e
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-06-25 13:32:20 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10x
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char k;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+	 _putchar(k);
+	 _putchar("\n");
+	}
+}
