3321c3aba2fcda8ad436ae1b54f267b06bbd604f
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-06-25 14:34:37 -0700

    ./7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @k: function parameter
+ * Return: i
+ */
+
+int print_last_digit(int k)
+{
+	int i;
+
+	i = k % 10;
+	if (k < 0)
+		i = -i;
+	_putchar(i + '0');
+	return (i);
+}
