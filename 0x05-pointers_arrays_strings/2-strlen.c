b86a5d1f76ae45ea0fce41b87b83788101bd9105
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-01 14:48:20 -0700

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -13,7 +13,8 @@ int _strlen(char *s)
 	/*declare the string first*/
 	int length = 0;
 	/*do not include the null terminator*/
-	for (; *s != '\0'; s++) /*Loop through each character in the string until the null terminator is reached*/
+	for (; *s != '\0'; s++) /*Loop through each character in the string*/
+				  /*until the null terminator is reached*/
 	{
 		length++; /*Increment the length counter*/
 	}
