3f861f88f08d62d1751250a609123fd8faf1e3f2
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-02 12:00:51 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string
+ * @str: the string to print
+ * we going to print this out in 2's,
+ * which means only even numbers.
+ * we increment this by 2, which means it will
+ * skip every odd number and print out even numbers.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2) /*skips character and print 2*/
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
