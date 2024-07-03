04d8e01a8dfdb6cc42507ffbbe07a605dea64559
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-03 04:19:11 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,10 +6,8 @@
  * @dest: destination
  * @src: The source string
  * Return: the pointer to dest
- */
-
-/**
- * Uses a for loop to copy each character from
+ *
+ * Uses a for loop to copy each character frm
  * src to dest until it encounters the null byte (\0).
  * After copying all characters, it explicitly adds the
  * null byte at the end of dest.
