30d73210912f08b68b706c7c1690c38ee3978e92
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-03 04:15:23 -0700

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,6 +8,14 @@
  * Return: the pointer to dest
  */
 
+/**
+ * Uses a for loop to copy each character from
+ * src to dest until it encounters the null byte (\0).
+ * After copying all characters, it explicitly adds the
+ * null byte at the end of dest.
+ * Returns the pointer to dest.
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i;
