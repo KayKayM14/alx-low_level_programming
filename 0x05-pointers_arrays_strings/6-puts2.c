23db83cff24a89c338aa02686ce3ded9ff68af60
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-03 01:44:52 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,9 +13,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2) /*skips character and print 2*/
+	for (i = 0; str[i] != '\0'; i ++) /*skips character and print 2*/
 	{
-		_putchar(str[i]);
+		if (i % 2 ==0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
