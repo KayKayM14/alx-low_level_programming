e17eb2f8a9cc0ce9b9c7fed94442f98bb85c8270
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-03 13:56:51 -0700

    ./1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenate two strings with a limit
+ * @dest: destination string
+ * @src: source string to be added to dest
+ * @n: max number of bytes to concatenate from src
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest); /*find the length of dest*/
+	int i;
+
+	/*copy up to n characters from src to dest*/
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = src[i]; /*copy each character from src to dest*/
+		src++; /*move to the next charac in src*/
+	}
+	dest[len + i] = '\0'; /*add the null to end the string*/
+	return (dest);
+}
