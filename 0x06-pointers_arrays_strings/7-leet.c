bc792445263ccea6abf44e2456d1a81a8d753794
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-04 15:43:16 -0700

    7. Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,22 @@
 char *leet(char *str)
 {
 	char *ptr = str; /*pointer to iterate through string*/
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_nums = "44337071";
+	int i;
+
 	/*iterate through each character in the string*/
 	for (; *ptr != '\0'; ptr++)
 	{
 		/*check each character and replace if necessary*/
-		if (*ptr == 'a' || *ptr == 'A')
-			*ptr = '4';
-		else if (*ptr == 'e' || *ptr == 'E')
-			*ptr = '3'; /*replace with 3*/
-		else if (*ptr == 'o' || *ptr == 'O')
-			*ptr = '0';
-		else if (*ptr == 't' || *ptr == 'T')
-			*ptr = '7';
-		else if (*ptr == 'l' || *ptr == 'L')
-			*ptr = '1';
+		for (i = 0; leet_chars[i] != '\0'; i++)
+		{
+			if (*ptr == leet_chars[i]) /*if values is ch*/
+			{
+				*ptr = leet_nums[i]; /*update to num*/
+			break; /*stop loop*/
+			}
+		}
 	}
 	return (str); /*return modified string*/
 }
