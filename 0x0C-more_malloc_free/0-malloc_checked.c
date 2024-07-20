8ffdfed018913bfca77e24db5b38c783f132a2ef
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-20 15:09:55 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,25 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - a function that allocates memory using malloc.
- * @b: pointer to memory
- * Return: allocated memory
+ * malloc_checked - allocated memory using malloc
+ * @b: size of memory
+ * Return: void
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	char *output;
 
-	/*allocate mem usin malloc*/
-	ptr = malloc(b);
-
-	/*check if malloc failed (ptr NULL)*/
-	if (ptr == NULL)
-	{
-		/*terminate the process with stats val 98*/
+	output = malloc(b);
+	if (output == NULL)
 		exit(98);
-	}
-
-	/*returin pointer to allocated memory*/
-	return (ptr);
+	else
+		return (output);
 }
