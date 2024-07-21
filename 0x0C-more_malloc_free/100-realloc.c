e8521f8c46b6597394cf010e670244dbff07455a
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-21 12:21:11 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,23 +1,21 @@
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _realloc - reallocates memory block
- * @ptr: ptr to the previously allocated mem
- * @old_size: size of the old mem block
- * @new_size: new size for the mem block
- *
- * Return: ptr to the newly allocated mem, or null if it fails
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of the allocated space for ptr
+ * @new_size: new size of the new memory block
+ * Return: pointer to the newly allocated memory, or NULL if it fails
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-	unsigned int min_size;
+	char *new_ptr;
+	unsigned int min_size, i;
 
 	/*Handle special cases*/
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -27,23 +25,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (malloc(new_size));
 	}
-
 	/*If new_size is equal to old_size, no need to reallocate*/
 	if (new_size == old_size)
 	{
-	return (ptr);
+		return (ptr);
 	}
 	/*Allocate new memory block*/
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
 	/*Copy data from old memory block to new memory block*/
 	min_size = old_size < new_size ? old_size : new_size;
-	memcpy(new_ptr, ptr, min_size);
-
-	/*free old mem block*/
+	for (i = 0; i < min_size; i++)
+	{
+		new_ptr[i] = ((char *)ptr)[i];
+	}
+	/*Free the old memory block*/
 	free(ptr);
 
 	return (new_ptr);
