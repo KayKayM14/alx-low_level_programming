58dd4a41d83ab280144e8ee029cb1ea669213ce9
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-07-24 14:05:00 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -13,3 +13,9 @@ void print_name(char *name, void (*f)(char *))
 	if (name && f)
 		f(name); /*Call the function pointed to by 'f' with 'name'*/
 }
+
+/**
+ * This function uses the function pointer `f` to print the given
+ * name in different formats. It first checks if both `name` and
+ * `f` are not NULL. If they are valid, it calls the function
+ * pointed to by `f` with `name` as the argumen
